b91aa8b8cbfd74fcffc8453848eef084eea4a2b2
Mac <mac@Macui-MacBookPro.local>
2022-07-03 23:44:10 +0900

    백준 코딩테스트 최초 커밋

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n > 0 && n <= 100000){
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            for(int i = n; i > 0; i--){
+                bw.write(i + "\n");
+            }
+            bw.flush();
+        }
+    }
+}
