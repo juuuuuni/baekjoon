7a4ee4b9478d4c545aa5b91a0a2f83c67306d6d5
mac <08300424>
2022-07-04 21:25:03 +0900

    2438.별찍기_1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < t; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());    //입력받는 곳.
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if(a > 0 && b < 10){
-                 bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " +(a + b) + "\n");
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n >= 1 && n <= 100){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
         }
-        }
-        bw.flush();
     }
 }
