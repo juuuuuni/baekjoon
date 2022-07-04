7ddb24daa04e82017a34eb4457e1a40642842f92
mac <08300424>
2022-07-04 21:14:32 +0900

    11022.A+B_8

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        if(n > 0 && n <= 100000){
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            for(int i = n; i > 0; i--){
-                 bw.write(i + "\n");
-            }
-            bw.flush();
+        int t = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < t; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());    //입력받는 곳.
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a > 0 && b < 10){
+                 bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " +(a + b) + "\n");
         }
+        }
+        bw.flush();
     }
 }
