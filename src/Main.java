577e68311ea7a2ac0d999ddee237853b116081f7
mac <08300424>
2022-07-05 22:05:16 +0900

    10952.A+B_5

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int x = Integer.parseInt(st1.nextToken());
-        String str = "";
-        for(int i = 0; i < n; i ++) {
-            int a = Integer.parseInt(st2.nextToken());
-            if(a < x){
-                str += a + " ";
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean chk = true;
+        while(chk){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a != 0 && b != 0){
+                if(a > 0 && b < 10){
+                    bw.write(a + b + "\n");
+                }
+            }else{
+                chk = false;
             }
         }
-        System.out.println(str.substring(0, (str.length() - 1)));
-        }
+        bw.flush();
+    }
+
     }
