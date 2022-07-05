5f7db715d9014d8e20d502f448b6c7820b350c20
mac <08300424>
2022-07-05 21:48:16 +0900

    10871.X보다 작은 수

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(n >= 1 && n <= 100){
-            for(int i = 0; i < n; i++){
-                for(int k = 0; k < ((n - 1) - i); k++){
-                    System.out.print(" ");
-                }
-                for(int j = 0; j <= i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println("");
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int x = Integer.parseInt(st1.nextToken());
+        String str = "";
+        for(int i = 0; i < n; i ++) {
+            int a = Integer.parseInt(st2.nextToken());
+            if(a < x){
+                str += a + " ";
             }
         }
+        System.out.println(str.substring(0, (str.length() - 1)));
+        }
     }
-}
