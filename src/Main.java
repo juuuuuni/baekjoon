0d3dca24a54ac4f3cf0e273a9a6cc1887b1535ab
mac <08300424>
2022-07-07 21:49:39 +0900

    1110.더하기 사이클

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = "";
-        while ((input = br.readLine()) != null && !input.isEmpty()){
-            StringTokenizer st = new StringTokenizer(input);
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if(a > 0 && b < 10){
-                bw.write(a + b + "\n");
+        int n = Integer.parseInt(br.readLine());
+        boolean chk = true;
+        int cycle = 0;
+        if(n >= 0 && n <= 99) {
+            String a = "";
+            if(n < 10){
+                a = String.format("%02d",n);
+            }else {
+                a = Integer.toString(n);
             }
+            while (chk) {
+                String b = a.substring(0,1);
+                String c = a.substring(1,2);
+               int i = Integer.parseInt(b) + Integer.parseInt(c);
+               if(i < 10){
+                   a = String.format("%02d",i);
+               }else {
+                   a = Integer.toString(i);
+                }
+               String d = a.substring(1,2);
+               a = c + d;
+               cycle++;
+               if(Integer.parseInt(a) == n){
+                   chk = false;
+               }
+            }
+            System.out.println(cycle);
         }
-        bw.flush();
     }
     }
