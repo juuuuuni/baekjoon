8844555c36663718cbc9f352f64dac28310cc0f1
AN.JUNHEE <an_junhee@radcns.com>
2022-07-08 23:09:25 +0900

    10818.최소,최대

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,23 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        boolean chk = true;
-        int cycle = 0;
-        if(n >= 0 && n <= 99) {
-            String a = "";
-            if(n < 10){
-                a = String.format("%02d",n);
-            }else {
-                a = Integer.toString(n);
-            }
-            while (chk) {
-                String b = a.substring(0,1);
-                String c = a.substring(1,2);
-               int i = Integer.parseInt(b) + Integer.parseInt(c);
-               if(i < 10){
-                   a = String.format("%02d",i);
-               }else {
-                   a = Integer.toString(i);
+        String a = br.readLine();
+        StringTokenizer st = new StringTokenizer(a);
+        int arr[] = new int[n];
+        if(n >= 1 && n <= 1000000){
+            for(int i = 0; i < n; i++){
+                int chk = Integer.parseInt(st.nextToken());
+                if(chk >= -1000000 && chk <= 1000000){
+                    arr[i] = chk;
                 }
-               String d = a.substring(1,2);
-               a = c + d;
-               cycle++;
-               if(Integer.parseInt(a) == n){
-                   chk = false;
-               }
             }
-            System.out.println(cycle);
+            Arrays.sort(arr);   //배열 정렬
+            System.out.println(arr[0]);
+            System.out.println(arr[n-1]);
         }
     }
     }
