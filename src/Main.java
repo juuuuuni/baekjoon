1f5645401788c3fb29e38db2068444cc86eb2f7a
AN.JUNHEE <an_junhee@radcns.com>
2022-07-10 00:24:46 +0900

    2577.숫자의 개수

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import java.io.*;
-import java.util.*;
-
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String a = br.readLine();
-        StringTokenizer st = new StringTokenizer(a);
-        int arr[] = new int[n];
-        if(n >= 1 && n <= 1000000){
-            for(int i = 0; i < n; i++){
-                int chk = Integer.parseInt(st.nextToken());
-                if(chk >= -1000000 && chk <= 1000000){
-                    arr[i] = chk;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        if(a >= 100 && a < 1000 && b >= 100 && b < 1000 && c >= 100 && c < 1000){
+            String str = Integer.toString(a * b * c);
+            String arr[] = str.split("");
+            int chk = 0;
+            for(int i = 0; i < 10; i++){
+                for(int k = 0; k < arr.length; k++){
+                    if(i == Integer.parseInt(arr[k])){
+                        chk++;
+                    }
                 }
+                System.out.println(chk);
+                chk = 0;
             }
-            Arrays.sort(arr);   //배열 정렬
-            System.out.println(arr[0]);
-            System.out.println(arr[n-1]);
         }
     }
     }
