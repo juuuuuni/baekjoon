f8e75841fd267621557f54dc163d505aa939e121
AN.JUNHEE <an_junhee@radcns.com>
2022-07-10 00:57:36 +0900

    3052.나머지

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-        if(a >= 100 && a < 1000 && b >= 100 && b < 1000 && c >= 100 && c < 1000){
-            String str = Integer.toString(a * b * c);
-            String arr[] = str.split("");
-            int chk = 0;
-            for(int i = 0; i < 10; i++){
-                for(int k = 0; k < arr.length; k++){
-                    if(i == Integer.parseInt(arr[k])){
-                        chk++;
-                    }
-                }
-                System.out.println(chk);
-                chk = 0;
-            }
+        int arr[] = new int[10];
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(br.readLine()) % 42;
+            set.add(arr[i]);
         }
-    }
+        System.out.println(set.size());
+        }
     }
