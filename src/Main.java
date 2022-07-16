126547fb672d16450cbf49c4425885dd907d5512
AN.JUNHEE <an_junhee@radcns.com>
2022-07-16 18:38:53 +0900

    1065.한수

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,42 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String args[]) {
-        ArrayList<String> selfnumber = new ArrayList<String>();
-        for(int i = 0; i < 10000; i++){
-             selfnumber.add(Integer.toString(i + 1));
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n >= 1 && n <= 1000){
+            System.out.println(x(n));
         }
-        for(int k = 0; k < 10000; k++){
-            int sum = d(k + 1);
-            selfnumber.remove(Integer.toString(sum));
-        }
-        for(int n = 0; n < selfnumber.size(); n++){
-            System.out.println(selfnumber.get(n));
-        }
+
     }
-    public static int d(int n){
-        int sum = n;
-        while (n > 0){
-            sum += n % 10;
-            n /= 10;
+
+    public static int x(int n) {
+        ArrayList<Integer> han = new ArrayList<Integer>();
+        if(n < 100) {
+            for (int i = 0; i < n; i++) {
+                han.add(i + 1);
+            }
+        }else {
+            for (int i = 0; i < 99; i++) {
+                han.add(i + 1);
+            }
+            for (int k = 100; k <= n; k++) {
+                ArrayList<Integer> ten = new ArrayList<Integer>();
+                int a = k;
+                while (a > 0) {
+                    ten.add(a % 10);
+                    a /= 10;
+                }
+                int chk = ten.get(1) - ten.get(0);
+                if (ten.size() == 3) {
+                    if ((ten.get(2) - ten.get(1)) == chk) {
+                        han.add(n);
+                    }
+                }
+            }
         }
-        return sum;
+        return han.size();
     }
     }
 
